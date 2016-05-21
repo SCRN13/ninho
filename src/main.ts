3bf61c40847bc2ba3c789cb4a31d5a2b04caeeac
Leander Dirkse <leander.dirkse@gmail.com>
2016-05-21 17:16:45 +0200

    Fix: koa -> http

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,16 @@
 /// <reference path="../typings/tsd.d.ts"/>
-let socketIO = require('socket.io');
-let koa = require('koa');
-let cors = require('koa-cors');
+let app  = require('express')();
+let http = require('http').Server( app );
+let io   = require('socket.io')(http);
 
-let sio = socketIO();
-// sio.serveClient(true); // the server will serve the client js file
-// sio.attach(httpServer);
-
-// listen for a connection
-sio.on('connection', function(socket) {
-	console.log('User ' + socket.id + ' connected');
-	socket.emit('message', 'Hello from the client');
+app.get( '/', function( req, res ) {
+    res.send('HELLO!');
 });
 
-
-let app = koa();
-app.use(function* () {
-	this.body = 'Hello World';
+io.on('connection', function(socket) {
+    console.log('connection');
 });
-app.use(cors({credentials: true}));
-sio.attach(app);
 
-
-app.listen(3000);
-
-console.log('Hello world');
+http.listen( 4000, function() {
+    console.log('listening...');
+});
