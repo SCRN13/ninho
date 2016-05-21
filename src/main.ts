6e6fde0f91634c4ccd1bcf8ce9b8593a7b168c87
Sjoerd Rappard <s.f.rappard@gmail.com>
2016-05-21 16:41:58 +0200

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/tsd.d.ts"/>
 let socketIO = require('socket.io');
 let koa = require('koa');
+let cors = require('koa-cors');
 
 let sio = socketIO();
 // sio.serveClient(true); // the server will serve the client js file
@@ -17,7 +18,10 @@ sio.on('connection', function(socket) {
 	// this.body = 'Hello World';
 // });
 let app = koa();
-app.listen(3000);
+app.use(cors());
 sio.attach(app);
+
+
+app.listen(3000);
 
 console.log('Hello world');
